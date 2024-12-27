d454fd77525b9ff780b289fcc6334b71cbf8e9e3
rifatshampod <shampodrifat@gmail.com>
2024-12-27 02:25:54 +0200

    initial migration added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    #id = models.AutoField()
+    name = models.CharField(max_length=155)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    file = models.FileField()
+    admin = models.BooleanField(default=0)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
